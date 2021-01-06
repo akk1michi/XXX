77b3d5c7653f81c86490262d184c9fe9f453c063
akk1michi <55663938+akk1michi@users.noreply.github.com>
2021-01-06 15:21:26 +0300

    Добавил и настроил камеру

--- Scripts/Characters/Player Character/Character.gd
+++ Scripts/Characters/Player Character/Character.gd
@@ -26,7 +26,6 @@ func _physics_process(delta):
 	if Input.is_action_pressed("Jump") and is_on_floor():
 		velocity.y=-jumpForce
 	velocity = move_and_slide(velocity,Vector2.UP)
-	print(velocity)
 	#if is_on_floor()
 		#velocity.y+=gravity
 	velocity.x=lerp(velocity.x,0,0.2)
