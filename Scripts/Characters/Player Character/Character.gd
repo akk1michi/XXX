4d5ea9d05ef9d548c77ed811fb7af84a94651cd2
akk1michi <55663938+akk1michi@users.noreply.github.com>
2021-01-06 13:04:16 +0300

    Добавил TileMaps.

--- Scripts/Characters/Player Character/Character.gd
+++ Scripts/Characters/Player Character/Character.gd
@@ -1,14 +1,24 @@
 extends KinematicBody2D
 var speed
-const maxspeed=300
-var gravity=50
-var jumpForce=1000
+const maxspeed=200
+var gravity=30
+var jumpForce=600
 var velocity=Vector2(0,0)
+var face=true
 func _physics_process(delta):
 	if Input.is_action_pressed("Right"):
+		face=true
 		velocity.x=maxspeed
-	if Input.is_action_pressed("Left"):
+		$AnimatedSprite.play("Run_Right")
+	elif Input.is_action_pressed("Left"):
+		face=false
 		velocity.x=-maxspeed
+		$AnimatedSprite.play("Run_Left")
+	elif face==true:
+		$AnimatedSprite.play("Idle_Right")
+	else:
+		$AnimatedSprite.play("Idle_Left")
+	
 	
 		
 		
@@ -16,7 +26,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("Jump") and is_on_floor():
 		velocity.y=-jumpForce
 	velocity = move_and_slide(velocity,Vector2.UP)
-	print(is_on_wall())
+	print(velocity)
 	#if is_on_floor()
 		#velocity.y+=gravity
 	velocity.x=lerp(velocity.x,0,0.2)
