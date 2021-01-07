f7c314980b3803f105d4fb14bd23972679b87907
akk1michi <55663938+akk1michi@users.noreply.github.com>
2021-01-07 14:37:55 +0300

    Добавил бекграунд и one-way платформы.
    Теперь с особых платформ можно спрыгивать нажав "S". Если зажать "S" в прыжке падая на one-way платформу, то она игнорируется.

--- Scripts/Characters/Player Character/Character.gd
+++ Scripts/Characters/Player Character/Character.gd
@@ -19,11 +19,14 @@ func _physics_process(delta):
 	else:
 		$AnimatedSprite.play("Idle_Left")
 	
-	
+	if Input.is_action_pressed("Down"):
+		set_collision_mask_bit(1,false)
+	else:
+		set_collision_mask_bit(1,true)
 		
 		
 	velocity.y+=gravity
-	if Input.is_action_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y=-jumpForce
 	velocity = move_and_slide(velocity,Vector2.UP)
 	#if is_on_floor()
