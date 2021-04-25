e4949cadc4d2d9e9a4cbd4f3055615a2015785a3
akk1michi <55663938+akk1michi@users.noreply.github.com>
2021-04-25 19:25:22 +0300

    Hp+фикс анимаций

--- Scenes/HUD.gd
+++ Scenes/HUD.gd
@@ -1,13 +1,43 @@
 extends CanvasLayer
 
 var coins = 0
-var health = 3
+var health =0
 
 func _ready():
+	health = $"../Character".health
+	$Heart.play("Full heart")
+	$Heart2.play("Full heart")
+	$Heart3.play("Full heart")
 	$Score.text= String(coins)
 	
+func _physics_process(delta):
+	health = $"../Character".health	
+	if (health==6):
+		$Heart.play("Full heart")
+		$Heart2.play("Full heart")
+		$Heart3.play("Full heart")
+	elif (health==5):
+		$Heart.play("Full heart")
+		$Heart2.play("Full heart")
+		$Heart3.play("Half heart")
+	elif (health==4):
+		$Heart.play("Full heart")
+		$Heart2.play("Full heart")
+		$Heart3.play("Empty heart")
+	elif (health==3):
+		$Heart.play("Full heart")
+		$Heart2.play("Half heart")
+		$Heart3.play("Empty heart")
+	elif (health==2):
+		$Heart.play("Full heart")
+		$Heart2.play("Empty heart")
+		$Heart3.play("Empty heart")
+	elif (health==1):
+		$Heart.play("Half heart")
+		$Heart2.play("Empty heart")
+		$Heart3.play("Empty heart")
+		
 	
- 
 
 
 func _on_coin_collected():
--- Scripts/Characters/Player Character/Character.gd
+++ Scripts/Characters/Player Character/Character.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 
 var coins=0
-var health = 3
+onready var health = 6
 
 var speed
 const maxspeed=200
@@ -18,32 +18,32 @@ var velocity=Vector2(0,0)
 var input_x=1
 var face=true
 func _physics_process(delta):
-	
 	if velocity.x>0:
 		$AnimationTree.set("parameters/Idle/blend_position",input_x)
 		$AnimationTree.set("parameters/Run/blend_position",input_x)
 		$AnimationTree.set("parameters/JumpStart/blend_position",input_x)
 		$AnimationTree.set("parameters/JumpLoop/blend_position",input_x)
 		$AnimationTree.set("parameters/Wall/blend_position",input_x)
-		$AnimationTree.get("parameters/playback").travel("Run")
 	elif velocity.x<0:
 		$AnimationTree.set("parameters/Idle/blend_position",input_x)
 		$AnimationTree.set("parameters/Run/blend_position",input_x)
 		$AnimationTree.set("parameters/JumpStart/blend_position",input_x)
 		$AnimationTree.set("parameters/JumpLoop/blend_position",input_x)
 		$AnimationTree.set("parameters/Wall/blend_position",input_x)
-		$AnimationTree.get("parameters/playback").travel("Run")
-	else:
-		$AnimationTree.get("parameters/playback").travel("Idle")
+		
+	
 	if Input.is_action_pressed("Right"):
+		$AnimationTree.get("parameters/playback").travel("Run")
 		face=true
 		velocity.x=maxspeed
 		input_x=1
-		
 	elif Input.is_action_pressed("Left"):
+		$AnimationTree.get("parameters/playback").travel("Run")
 		face=false
 		velocity.x=-maxspeed
 		input_x=-1
+	else:
+		$AnimationTree.get("parameters/playback").travel("Idle")
 		
 	
 	
@@ -61,7 +61,7 @@ func _physics_process(delta):
 	if !is_on_floor():
 		$AnimationTree.get("parameters/playback").travel("JumpLoop")
 		
-	if Input.is_action_pressed("Jump")&&is_on_floor()&&!is_on_wall():
+	if Input.is_action_pressed("Jump")&&is_on_floor():
 			velocity.y=-jumpForce
 			
 	if is_on_wall() && Input.is_action_pressed("Right")&&!is_on_floor()&&Input.is_action_just_pressed("Jump"):
